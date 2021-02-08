b4aaef58c93e7766dd39774f064987feace0f9e1
ghoeugenio <ghoeugenio@gmail.com>
2021-02-08 15:29:59 -0300

    navigation e db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 
-import { NavigationContainer, DrawerActions, useNavigation } from '@react-navigation/native';
-import {
-    createDrawerNavigator,
-    DrawerContentScrollView,
-    DrawerItemList,
-    DrawerItem,
-} from '@react-navigation/drawer';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 
-import { Appbar } from 'react-native-paper';
+import { createStackNavigator } from '@react-navigation/stack'
 
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
@@ -16,58 +10,34 @@ import { StatusBar, Text, TouchableOpacity, Button, View } from 'react-native';
 
 import Home from './pages/Home';
 import Cadastro from './pages/Cadastros';
-import style from './pages/Home/style';
 
-const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-function Feed({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Feed Screen</Text>
-            <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
-            <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
-        </View>
-    );
-}
-
-function CustomDrawerContent(props) {
-    return (
-        <DrawerContentScrollView {...props}>
-            <DrawerItemList {...props} />
-            <DrawerItem
-                label="Close drawer"
-                onPress={() => props.navigation.closeDrawer()}
-            />
-            <DrawerItem
-                label="Toggle drawer"
-                onPress={() => props.navigation.toggleDrawer()}
-            />
-        </DrawerContentScrollView>
-    );
-}
-
-function MyDrawer() {
-    return (
-        <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Cadastro" component={Feed} />
-        </Drawer.Navigator>
-    );
-}
-
-function Bar({ navigation }) {
+function MyStack() {
 
     return (
-        <Appbar.Header style={style.appbar}>
 
-            <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-                <Text>Botao</Text>
-            </TouchableOpacity>
+        <Stack.Navigator>
 
-        </Appbar.Header>
+            <Stack.Screen
+                name="EDIAPP"
+                component={Home}
+                options={{
+                    headerTitleAlign: 'center',
+                    headerTintColor: '#212121',
+                    headerStyle: { backgroundColor: '#ffeb3b' }
+                }} />
+
+            <Stack.Screen
+                name="Cadastro"
+                component={Cadastro}
+                options={{
+                    headerTintColor: '#000'
+                }} />
+
+        </Stack.Navigator>
     );
 }
-
 
 export default function App() {
 
@@ -75,11 +45,13 @@ export default function App() {
 
         <NavigationContainer>
 
-            <StatusBar barStyle="light-content" hidden={false} backgroundColor="#101010" translucent={true} />
+            <StatusBar
+                barStyle="light-content"
+                hidden={false}
+                backgroundColor="#101010"
+                translucent={true} />
 
-            <Bar />
-
-            <MyDrawer />
+            <MyStack />
 
         </NavigationContainer>
     );
